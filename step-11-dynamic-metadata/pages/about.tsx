7d5102ef6456dccf64101a97663819a51d049f56
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2022-07-06 01:07:00 +0500

    Completed - step 11 - Dynamic Metadata

---
+++ step-11-dynamic-metadata/pages/about.tsx
@@ -0,0 +1,29 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import styles from '../styles/Home.module.css'
+import Widget from "../components/Widget";
+
+const About:NextPage = () => {
+    return (
+        <>
+        <Head>
+            <title>About this website</title>
+        </Head>
+        <div className={styles.container}>
+            <h1>This is  About page</h1>
+            <Link href={'/'} passHref>
+                <a className={styles.btn}>
+                    Back to Home
+                </a>
+            </Link>
+            <br />
+            <small>See the title of the page</small>
+        </div>
+        <br />
+        <Widget pageName="about" />
+        </>
+    )
+}
+
+export default About;
